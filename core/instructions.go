c0d5f3c1ecf36e4f660d01fc7aa91eba884ce36e
Michael Pratt <michael@pratt.im>
2013-06-28 22:56:56 -0700

    Move shift instructions and tests to dedicated file

--- core/instructions.go
+++ core/instructions.go
@@ -13,34 +13,3 @@ type InstrFields struct {
     Rm uint8
     Rn uint8
 }
-
-type Lsl InstrFields
-type Lsr InstrFields
-
-func LslImm16(instr FetchedInstr) DecodedInstr {
-    raw_instr := instr.Uint32()
-
-    Rd := uint8(raw_instr & 0x7)
-    Rm := uint8((raw_instr >> 3) & 0x7)
-    Imm := (raw_instr >> 6) & 0x1f
-
-    return Lsl{Rd: Rd, Rm: Rm, Imm: Imm, S: true, Rn: 0}
-}
-
-func LsrImm16(instr FetchedInstr) DecodedInstr {
-    raw_instr := instr.Uint32()
-
-    Rd := uint8(raw_instr & 0x7)
-    Rm := uint8((raw_instr >> 3) & 0x7)
-    Imm := (raw_instr >> 6) & 0x1f
-
-    return Lsr{Rd: Rd, Rm: Rm, Imm: Imm, S: true, Rn: 0}
-}
-
-func (instr Lsl) Execute(regs *Registers) {
-    regs.R[instr.Rd] = regs.R[instr.Rm] << instr.Imm
-}
-
-func (instr Lsr) Execute(regs *Registers) {
-    regs.R[instr.Rd] = regs.R[instr.Rm] >> instr.Imm
-}
---
+++ core/instructions_test.go
@@ -0,0 +1,48 @@
+package core
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Identify type of instruction
+// Don't check decoded values
+type IdentifyCase struct {
+	instr       FetchedInstr
+	instr_valid bool
+}
+
+// Decode instruction internals
+// Given known instruction
+type DecodeCase struct {
+	instr   FetchedInstr
+	decoded DecodedInstr
+}
+
+func verify_identify(t *testing.T, test IdentifyCase, instr_type reflect.Type, identified DecodedInstr, err error) {
+	if err != nil {
+		if test.instr_valid {
+			t.Errorf("instr: %v, err: %v", test.instr, err)
+		} else {
+            /* An error is OK, because this wasn't considered a valid instruction */
+			return
+		}
+	}
+
+    identified_type := reflect.TypeOf(identified)
+    types_match := instr_type == identified_type
+
+    if test.instr_valid && !types_match {
+        t.Errorf("instr: %v, decoded type: %T, expected type: %v", test.instr, identified, instr_type)
+    }
+
+    if !test.instr_valid && types_match {
+        t.Errorf("instr: %v, decoded type: %T, expected not type: %v", test.instr, identified, instr_type)
+    }
+}
+
+func verify_decode(t *testing.T, test DecodeCase, actual DecodedInstr) {
+	if actual != test.decoded {
+		t.Errorf("instr: %v, expected %#v, got %#v", test.instr, test.decoded, actual)
+	}
+}
