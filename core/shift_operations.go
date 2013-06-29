daa9a938888a95f44ecb681fee7296479a2aea28
Michael Pratt <michael@pratt.im>
2013-06-29 11:51:28 -0700

    Make shift operations generic
    This can be reused for LSR

--- core/shift_operations.go
+++ core/shift_operations.go
@@ -1,14 +1,16 @@
 package core
 
-/* Perform LSL instruction, updating condition codes */
-func LSL(regs *Registers, value uint32, shift_n uint8, S bool) uint32 {
+type ShiftFunc func (uint32, uint8) (uint32, bool)
+
+/* Perform shift operation, updating condition codes */
+func ShiftOp(regs *Registers, value uint32, shift_n uint8, S bool, do_shift ShiftFunc) uint32 {
 	var result uint32
 	var carry_out bool
 
 	if shift_n == 0 {
 		result, carry_out = value, regs.Apsr.C
 	} else {
-		result, carry_out = LSL_C(value, shift_n)
+		result, carry_out = do_shift(value, shift_n)
 	}
 
 	if S {
@@ -18,6 +20,11 @@ func LSL(regs *Registers, value uint32, shift_n uint8, S bool) uint32 {
 	}
 
 	return result
+}
+
+/* Perform LSL instruction, updating condition codes */
+func LSL(regs *Registers, value uint32, shift_n uint8, S bool) uint32 {
+	return ShiftOp(regs, value, shift_n, S, LSL_C)
 }
 
 /* Left shift value by a positive amount */
