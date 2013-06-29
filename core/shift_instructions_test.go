f7a0e77a71a37a477d9e614cad2074e2a6005717
Michael Pratt <michael@pratt.im>
2013-06-29 01:05:20 -0700

    Add field names to test cases

--- core/shift_instructions_test.go
+++ core/shift_instructions_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestIdentifyLslImm(t *testing.T) {
 	cases := []IdentifyCase{
-		{FetchedInstr16(0x0000), true},  // lsl r0, r0, #0
-		{FetchedInstr16(0x01e7), true},  // lsl r7, r4, #7
-		{FetchedInstr16(0x4080), false}, // lsl r0, r0, r0
-		{FetchedInstr16(0x40bf), false}, // lsl r7, r7, r7
-		{FetchedInstr16(0x09e7), false}, // lsr r7, r4, #7
-		{FetchedInstr16(0xffff), false},
+		{instr: FetchedInstr16(0x0000), instr_valid: true},  // lsl r0, r0, #0
+		{instr: FetchedInstr16(0x01e7), instr_valid: true},  // lsl r7, r4, #7
+		{instr: FetchedInstr16(0x4080), instr_valid: false}, // lsl r0, r0, r0
+		{instr: FetchedInstr16(0x40bf), instr_valid: false}, // lsl r7, r7, r7
+		{instr: FetchedInstr16(0x09e7), instr_valid: false}, // lsr r7, r4, #7
+		{instr: FetchedInstr16(0xffff), instr_valid: false},
 	}
 
 	test_identify(t, cases, reflect.TypeOf(LslImm{}))
@@ -21,11 +21,11 @@ func TestIdentifyLslImm(t *testing.T) {
 func TestDecodeLslImm16(t *testing.T) {
 	cases := []DecodeCase{
 		// lsl r0, r0, #0
-		{FetchedInstr16(0x0000), LslImm{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
+		{instr: FetchedInstr16(0x0000), decoded: LslImm{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
 		// lsl r0, r0, #1
-		{FetchedInstr16(0x0040), LslImm{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
+		{instr: FetchedInstr16(0x0040), decoded: LslImm{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
 		// lsl r7, r4, #7
-		{FetchedInstr16(0x01e7), LslImm{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
+		{instr: FetchedInstr16(0x01e7), decoded: LslImm{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
 	}
 
 	test_decode(t, cases, LslImm16)
@@ -53,12 +53,12 @@ func TestExecuteLslImm(t *testing.T) {
 
 func TestIdentifyLslReg(t *testing.T) {
 	cases := []IdentifyCase{
-		{FetchedInstr16(0x4080), true},  // lsl r0, r0, r0
-		{FetchedInstr16(0x40bf), true},  // lsl r7, r7, r7
-		{FetchedInstr16(0x0000), false}, // lsl r0, r0, #0
-		{FetchedInstr16(0x01e7), false}, // lsl r7, r4, #7
-		{FetchedInstr16(0x09e7), false}, // lsr r7, r4, #7
-		{FetchedInstr16(0xffff), false},
+		{instr: FetchedInstr16(0x4080), instr_valid: true},  // lsl r0, r0, r0
+		{instr: FetchedInstr16(0x40bf), instr_valid: true},  // lsl r7, r7, r7
+		{instr: FetchedInstr16(0x0000), instr_valid: false}, // lsl r0, r0, #0
+		{instr: FetchedInstr16(0x01e7), instr_valid: false}, // lsl r7, r4, #7
+		{instr: FetchedInstr16(0x09e7), instr_valid: false}, // lsr r7, r4, #7
+		{instr: FetchedInstr16(0xffff), instr_valid: false},
 	}
 
 	test_identify(t, cases, reflect.TypeOf(LslReg{}))
@@ -67,11 +67,11 @@ func TestIdentifyLslReg(t *testing.T) {
 func TestDecodeLslReg16(t *testing.T) {
 	cases := []DecodeCase{
 		// lsl r0, r0, r0
-		{FetchedInstr16(0x4080), LslReg{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
+		{instr: FetchedInstr16(0x4080), decoded: LslReg{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
 		// lsl r7, r7, r7
-		{FetchedInstr16(0x40bf), LslReg{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
+		{instr: FetchedInstr16(0x40bf), decoded: LslReg{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
 		// lsl r4, r4, r7
-		{FetchedInstr16(0x40bc), LslReg{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
+		{instr: FetchedInstr16(0x40bc), decoded: LslReg{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
 	}
 
 	test_decode(t, cases, LslReg16)
@@ -104,13 +104,13 @@ func TestExecuteLslReg(t *testing.T) {
 
 func TestIdentifyLsrImm(t *testing.T) {
 	cases := []IdentifyCase{
-		{FetchedInstr16(0x0800), true},  // lsr r0, r0, #0
-		{FetchedInstr16(0x09e7), true},  // lsr r7, r4, #7
-		{FetchedInstr16(0x40c0), false}, // lsr r0, r0, r0
-		{FetchedInstr16(0x40d3), false}, // lsr r3, r3, r1
-		{FetchedInstr16(0x01e7), false}, // lsl r7, r4, #7
-		{FetchedInstr16(0x0000), false}, // lsl r0, r0, #0
-		{FetchedInstr16(0xffff), false},
+		{instr: FetchedInstr16(0x0800), instr_valid: true},  // lsr r0, r0, #0
+		{instr: FetchedInstr16(0x09e7), instr_valid: true},  // lsr r7, r4, #7
+		{instr: FetchedInstr16(0x40c0), instr_valid: false}, // lsr r0, r0, r0
+		{instr: FetchedInstr16(0x40d3), instr_valid: false}, // lsr r3, r3, r1
+		{instr: FetchedInstr16(0x01e7), instr_valid: false}, // lsl r7, r4, #7
+		{instr: FetchedInstr16(0x0000), instr_valid: false}, // lsl r0, r0, #0
+		{instr: FetchedInstr16(0xffff), instr_valid: false},
 	}
 
 	test_identify(t, cases, reflect.TypeOf(LsrImm{}))
@@ -119,11 +119,11 @@ func TestIdentifyLsrImm(t *testing.T) {
 func TestDecodeLsrImm16(t *testing.T) {
 	cases := []DecodeCase{
 		// lsr r0, r0, #0
-		{FetchedInstr16(0x0800), LsrImm{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
+		{instr: FetchedInstr16(0x0800), decoded: LsrImm{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
 		// lsr r0, r0, #1
-		{FetchedInstr16(0x0840), LsrImm{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
+		{instr: FetchedInstr16(0x0840), decoded: LsrImm{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
 		// lsr r7, r4, #7
-		{FetchedInstr16(0x09e7), LsrImm{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
+		{instr: FetchedInstr16(0x09e7), decoded: LsrImm{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
 	}
 
 	test_decode(t, cases, LsrImm16)
@@ -151,13 +151,13 @@ func TestExecuteLsrImm(t *testing.T) {
 
 func TestIdentifyLsrReg(t *testing.T) {
 	cases := []IdentifyCase{
-		{FetchedInstr16(0x40c0), true},  // lsr r0, r0, r0
-		{FetchedInstr16(0x40d3), true},  // lsr r3, r3, r1
-		{FetchedInstr16(0x0800), false}, // lsr r0, r0, #0
-		{FetchedInstr16(0x09e7), false}, // lsr r7, r4, #7
-		{FetchedInstr16(0x01e7), false}, // lsl r7, r4, #7
-		{FetchedInstr16(0x0000), false}, // lsl r0, r0, #0
-		{FetchedInstr16(0xffff), false},
+		{instr: FetchedInstr16(0x40c0), instr_valid: true},  // lsr r0, r0, r0
+		{instr: FetchedInstr16(0x40d3), instr_valid: true},  // lsr r3, r3, r1
+		{instr: FetchedInstr16(0x0800), instr_valid: false}, // lsr r0, r0, #0
+		{instr: FetchedInstr16(0x09e7), instr_valid: false}, // lsr r7, r4, #7
+		{instr: FetchedInstr16(0x01e7), instr_valid: false}, // lsl r7, r4, #7
+		{instr: FetchedInstr16(0x0000), instr_valid: false}, // lsl r0, r0, #0
+		{instr: FetchedInstr16(0xffff), instr_valid: false},
 	}
 
 	test_identify(t, cases, reflect.TypeOf(LsrReg{}))
@@ -166,11 +166,11 @@ func TestIdentifyLsrReg(t *testing.T) {
 func TestDecodeLsrReg16(t *testing.T) {
 	cases := []DecodeCase{
 		// lsr r0, r0, r0
-		{FetchedInstr16(0x40c0), LsrReg{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
+		{instr: FetchedInstr16(0x40c0), decoded: LsrReg{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
 		// lsr r7, r7, r7
-		{FetchedInstr16(0x40ff), LsrReg{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
+		{instr: FetchedInstr16(0x40ff), decoded: LsrReg{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
 		// lsr r4, r4, r7
-		{FetchedInstr16(0x40fc), LsrReg{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
+		{instr: FetchedInstr16(0x40fc), decoded: LsrReg{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
 	}
 
 	test_decode(t, cases, LsrReg16)
