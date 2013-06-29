4e6a3dc7ad1763130e719d3a6b9578dc64140180
Michael Pratt <michael@pratt.im>
2013-06-28 23:50:58 -0700

    Split immediate and register instruction types
    Treat them as separate instructions entirely, since their execute
    behaves differently.

--- core/shift_instructions.go
+++ core/shift_instructions.go
@@ -1,8 +1,8 @@
 package core
 
-/* LSL - Logical Shift Left
- * ARM ARM A7.7.67 - A7.7.68 */
-type Lsl InstrFields
+/* LSL - Logical Shift Left (immediate)
+ * ARM ARM A7.7.67 */
+type LslImm InstrFields
 
 func LslImm16(instr FetchedInstr) DecodedInstr {
     raw_instr := instr.Uint32()
@@ -11,8 +11,16 @@ func LslImm16(instr FetchedInstr) DecodedInstr {
     Rm := uint8((raw_instr >> 3) & 0x7)
     Imm := (raw_instr >> 6) & 0x1f
 
-    return Lsl{Rd: Rd, Rm: Rm, Imm: Imm, S: true, Rn: 0}
+    return LslImm{Rd: Rd, Rm: Rm, Imm: Imm, S: true, Rn: 0}
 }
+
+func (instr LslImm) Execute(regs *Registers) {
+    regs.R[instr.Rd] = regs.R[instr.Rm] << instr.Imm
+}
+
+/* LSL - Logical Shift Left (register)
+ * ARM ARM A7.7.68 */
+type LslReg InstrFields
 
 func LslReg16(instr FetchedInstr) DecodedInstr {
     raw_instr := instr.Uint32()
@@ -20,16 +28,16 @@ func LslReg16(instr FetchedInstr) DecodedInstr {
     Rdn := uint8(raw_instr & 0x7)
     Rm := uint8((raw_instr >> 3) & 0x7)
 
-    return Lsl{Rd: Rdn, Rn: Rdn, Rm: Rm, Imm: 0, S: true}
+    return LslReg{Rd: Rdn, Rn: Rdn, Rm: Rm, Imm: 0, S: true}
 }
 
-func (instr Lsl) Execute(regs *Registers) {
-    regs.R[instr.Rd] = regs.R[instr.Rm] << instr.Imm
+func (instr LslReg) Execute(regs *Registers) {
+    regs.R[instr.Rd] = regs.R[instr.Rn] << uint8(regs.R[instr.Rm])
 }
 
-/* LSR - Logical Shift Right
- * ARM ARM A7.7.69 - A7.7.69 */
-type Lsr InstrFields
+/* LSR - Logical Shift Right (immediate)
+ * ARM ARM A7.7.69 */
+type LsrImm InstrFields
 
 func LsrImm16(instr FetchedInstr) DecodedInstr {
     raw_instr := instr.Uint32()
@@ -38,8 +46,16 @@ func LsrImm16(instr FetchedInstr) DecodedInstr {
     Rm := uint8((raw_instr >> 3) & 0x7)
     Imm := (raw_instr >> 6) & 0x1f
 
-    return Lsr{Rd: Rd, Rm: Rm, Imm: Imm, S: true, Rn: 0}
+    return LsrImm{Rd: Rd, Rm: Rm, Imm: Imm, S: true, Rn: 0}
 }
+
+func (instr LsrImm) Execute(regs *Registers) {
+    regs.R[instr.Rd] = regs.R[instr.Rm] >> instr.Imm
+}
+
+/* LSR - Logical Shift Right (register)
+ * ARM ARM A7.7.70 */
+type LsrReg InstrFields
 
 func LsrReg16(instr FetchedInstr) DecodedInstr {
     raw_instr := instr.Uint32()
@@ -47,9 +63,9 @@ func LsrReg16(instr FetchedInstr) DecodedInstr {
     Rdn := uint8(raw_instr & 0x7)
     Rm := uint8((raw_instr >> 3) & 0x7)
 
-    return Lsr{Rd: Rdn, Rn: Rdn, Rm: Rm, Imm: 0, S: true}
+    return LsrReg{Rd: Rdn, Rn: Rdn, Rm: Rm, Imm: 0, S: true}
 }
 
-func (instr Lsr) Execute(regs *Registers) {
-    regs.R[instr.Rd] = regs.R[instr.Rm] >> instr.Imm
+func (instr LsrReg) Execute(regs *Registers) {
+    regs.R[instr.Rd] = regs.R[instr.Rn] >> uint8(regs.R[instr.Rm])
 }
--- core/shift_instructions_test.go
+++ core/shift_instructions_test.go
@@ -5,17 +5,17 @@ import (
 	"testing"
 )
 
-func TestIdentifyLsl(t *testing.T) {
+func TestIdentifyLslImm(t *testing.T) {
 	cases := []IdentifyCase{
 		{FetchedInstr16(0x0000), true}, // lsl r0, r0, #0
 		{FetchedInstr16(0x01e7), true}, // lsl r7, r4, #7
-		{FetchedInstr16(0x4080), true}, // lsl r0, r0, r0
-		{FetchedInstr16(0x40bf), true}, // lsl r7, r7, r7
+		{FetchedInstr16(0x4080), false},// lsl r0, r0, r0
+		{FetchedInstr16(0x40bf), false},// lsl r7, r7, r7
 		{FetchedInstr16(0x09e7), false},// lsr r7, r4, #7
 		{FetchedInstr16(0xffff), false},
 	}
 
-	instr_type := reflect.TypeOf(Lsl{})
+	instr_type := reflect.TypeOf(LslImm{})
 
 	for _, test := range cases {
 		instr, err := test.instr.Decode()
@@ -26,11 +26,11 @@ func TestIdentifyLsl(t *testing.T) {
 func TestDecodeLslImm16(t *testing.T) {
 	cases := []DecodeCase{
         // lsl r0, r0, #0
-		{FetchedInstr16(0x0000), Lsl{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
+		{FetchedInstr16(0x0000), LslImm{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
         // lsl r0, r0, #1
-		{FetchedInstr16(0x0040), Lsl{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
+		{FetchedInstr16(0x0040), LslImm{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
         // lsl r7, r4, #7
-		{FetchedInstr16(0x01e7), Lsl{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
+		{FetchedInstr16(0x01e7), LslImm{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
 	}
 
 	for _, test := range cases {
@@ -39,14 +39,32 @@ func TestDecodeLslImm16(t *testing.T) {
 	}
 }
 
+func TestIdentifyLslReg(t *testing.T) {
+	cases := []IdentifyCase{
+		{FetchedInstr16(0x4080), true}, // lsl r0, r0, r0
+		{FetchedInstr16(0x40bf), true}, // lsl r7, r7, r7
+		{FetchedInstr16(0x0000), false},// lsl r0, r0, #0
+		{FetchedInstr16(0x01e7), false},// lsl r7, r4, #7
+		{FetchedInstr16(0x09e7), false},// lsr r7, r4, #7
+		{FetchedInstr16(0xffff), false},
+	}
+
+	instr_type := reflect.TypeOf(LslReg{})
+
+	for _, test := range cases {
+		instr, err := test.instr.Decode()
+		verify_identify(t, test, instr_type, instr, err)
+	}
+}
+
 func TestDecodeLslReg16(t *testing.T) {
 	cases := []DecodeCase{
         // lsl r0, r0, r0
-        {FetchedInstr16(0x4080), Lsl{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
+        {FetchedInstr16(0x4080), LslReg{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
         // lsl r7, r7, r7
-        {FetchedInstr16(0x40bf), Lsl{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
+        {FetchedInstr16(0x40bf), LslReg{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
         // lsl r4, r4, r7
-        {FetchedInstr16(0x40bc), Lsl{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
+        {FetchedInstr16(0x40bc), LslReg{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
 	}
 
 	for _, test := range cases {
@@ -55,18 +73,18 @@ func TestDecodeLslReg16(t *testing.T) {
 	}
 }
 
-func TestIdentifyLsr(t *testing.T) {
+func TestIdentifyLsrImm(t *testing.T) {
 	cases := []IdentifyCase{
 		{FetchedInstr16(0x0800), true}, // lsr r0, r0, #0
 		{FetchedInstr16(0x09e7), true}, // lsr r7, r4, #7
-		{FetchedInstr16(0x40c0), true}, // lsr r0, r0, r0
-		{FetchedInstr16(0x40d3), true}, // lsr r3, r3, r1
+		{FetchedInstr16(0x40c0), false},// lsr r0, r0, r0
+		{FetchedInstr16(0x40d3), false},// lsr r3, r3, r1
 		{FetchedInstr16(0x01e7), false},// lsl r7, r4, #7
 		{FetchedInstr16(0x0000), false},// lsl r0, r0, #0
 		{FetchedInstr16(0xffff), false},
 	}
 
-	instr_type := reflect.TypeOf(Lsr{})
+	instr_type := reflect.TypeOf(LsrImm{})
 
 	for _, test := range cases {
 		instr, err := test.instr.Decode()
@@ -77,11 +95,11 @@ func TestIdentifyLsr(t *testing.T) {
 func TestDecodeLsrImm16(t *testing.T) {
 	cases := []DecodeCase{
         // lsr r0, r0, #0
-		{FetchedInstr16(0x0800), Lsr{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
+		{FetchedInstr16(0x0800), LsrImm{Rd: 0, Rm: 0, Imm: 0, S: true, Rn: 0}},
         // lsr r0, r0, #1
-		{FetchedInstr16(0x0840), Lsr{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
+		{FetchedInstr16(0x0840), LsrImm{Rd: 0, Rm: 0, Imm: 1, S: true, Rn: 0}},
         // lsr r7, r4, #7
-		{FetchedInstr16(0x09e7), Lsr{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
+		{FetchedInstr16(0x09e7), LsrImm{Rd: 7, Rm: 4, Imm: 7, S: true, Rn: 0}},
 	}
 
 	for _, test := range cases {
@@ -90,14 +108,33 @@ func TestDecodeLsrImm16(t *testing.T) {
 	}
 }
 
+func TestIdentifyLsrReg(t *testing.T) {
+	cases := []IdentifyCase{
+		{FetchedInstr16(0x40c0), true}, // lsr r0, r0, r0
+		{FetchedInstr16(0x40d3), true}, // lsr r3, r3, r1
+		{FetchedInstr16(0x0800), false},// lsr r0, r0, #0
+		{FetchedInstr16(0x09e7), false},// lsr r7, r4, #7
+		{FetchedInstr16(0x01e7), false},// lsl r7, r4, #7
+		{FetchedInstr16(0x0000), false},// lsl r0, r0, #0
+		{FetchedInstr16(0xffff), false},
+	}
+
+	instr_type := reflect.TypeOf(LsrReg{})
+
+	for _, test := range cases {
+		instr, err := test.instr.Decode()
+		verify_identify(t, test, instr_type, instr, err)
+	}
+}
+
 func TestDecodeLsrReg16(t *testing.T) {
 	cases := []DecodeCase{
         // lsr r0, r0, r0
-        {FetchedInstr16(0x40c0), Lsr{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
+        {FetchedInstr16(0x40c0), LsrReg{Rd: 0, Rn: 0, Rm: 0, Imm: 0, S: true}},
         // lsr r7, r7, r7
-        {FetchedInstr16(0x40ff), Lsr{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
+        {FetchedInstr16(0x40ff), LsrReg{Rd: 7, Rn: 7, Rm: 7, Imm: 0, S: true}},
         // lsr r4, r4, r7
-        {FetchedInstr16(0x40fc), Lsr{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
+        {FetchedInstr16(0x40fc), LsrReg{Rd: 4, Rn: 4, Rm: 7, Imm: 0, S: true}},
 	}
 
 	for _, test := range cases {
